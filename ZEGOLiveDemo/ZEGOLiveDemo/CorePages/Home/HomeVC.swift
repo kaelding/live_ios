10b869b64f7e35dac717701126147d03e02a7259
Kael Ding <415676243@qq.com>
2021-12-24 10:43:45 +0800

    ✨ Add login view controller.

--- ZEGOLiveDemo/ZEGOLiveDemo/CorePages/Home/HomeVC.swift
+++ ZEGOLiveDemo/ZEGOLiveDemo/CorePages/Home/HomeVC.swift
@@ -12,28 +12,36 @@ class HomeVC: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var moreButton: UIButton!
-    @IBOutlet weak var signUpButton: UIButton!
-    @IBOutlet weak var contactButton: UIButton!
+    
+    @IBOutlet weak var moreButton: UIButton! {
+        didSet {
+            moreButton.layer.cornerRadius = 8.0
+            moreButton.clipsToBounds = true
+        }
+    }
+    
+    @IBOutlet weak var signUpButton: UIButton! {
+        didSet {
+            signUpButton.layer.cornerRadius = 8.0
+            signUpButton.clipsToBounds = true
+        }
+    }
+    
+    @IBOutlet weak var contactButton: UIButton! {
+        didSet {
+            contactButton.layer.cornerRadius = 8.0
+            contactButton.clipsToBounds = true
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        configUI()
-    }
-
-    func configUI() {
-        
-        moreButton.layer.cornerRadius = 8.0
-        moreButton.clipsToBounds = true
-        signUpButton.layer.cornerRadius = 8.0
-        signUpButton.clipsToBounds = true
-        contactButton.layer.cornerRadius = 8.0
-        contactButton.clipsToBounds = true
     }
     
     // MARK: - Action
+    
     @IBAction func moreButtonClick(_ sender: UIButton) {
         
     }
---
+++ ZEGOLiveDemo/ZEGOLiveDemo/CorePages/Login/LoginVC.swift
@@ -0,0 +1,109 @@
+//
+//  LoginVC.swift
+//  ZEGOLiveDemo
+//
+//  Created by Kael Ding on 2021/12/23.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userIDTextField: UITextField! {
+        didSet {
+            let userIDLeftView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: 49))
+            let userIDRightView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: 49))
+            userIDTextField.leftView = userIDLeftView
+            userIDTextField.leftViewMode = .always
+            userIDTextField.rightView = userIDRightView
+            userIDTextField.rightViewMode = .always
+            userIDTextField.layer.cornerRadius = 24.5
+            userIDTextField.attributedPlaceholder = NSAttributedString(string: "User ID",
+                                                                       attributes: [.foregroundColor: UIColor(named: "white_40%")!])
+        }
+    }
+    @IBOutlet weak var userNameTextField: UITextField! {
+        didSet {
+            let userNameLeftView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: 49))
+            let userNameRightView = UIView(frame: CGRect(x: 0, y: 0, width: 24, height: 49))
+            userNameTextField.leftView = userNameLeftView
+            userNameTextField.leftViewMode = .always
+            userNameTextField.rightView = userNameRightView
+            userNameTextField.rightViewMode = .always
+            userNameTextField.layer.cornerRadius = 24.5
+            userNameTextField.attributedPlaceholder = NSAttributedString(string: "User Name",
+                                                                         attributes: [.foregroundColor: UIColor(named: "white_40%")!])
+        }
+    }
+    @IBOutlet weak var loginButton: UIButton! {
+        didSet {
+            loginButton.layer.cornerRadius = 24.5
+            let layer = CAGradientLayer()
+            layer.startPoint = CGPoint(x: 0, y: 0)
+            layer.endPoint = CGPoint(x: 1, y: 0)
+            layer.locations = [NSNumber(value: 0.5), NSNumber(value: 1.0)]
+            let startColor = UIColor(named: "startColor") ?? UIColor.white
+            let endColor = UIColor(named: "endColor") ?? UIColor.white
+            layer.colors = [startColor.cgColor, endColor.cgColor]
+            layer.frame = loginButton.bounds
+            loginButton.layer.addSublayer(layer)
+        }
+    }
+    
+    var myUserID: String = ""
+    var myUserName: String = ""
+    
+    
+    // MARK: - lifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        configUI()
+    }
+    
+    func configUI() {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        let window : UIWindow = UIApplication.shared.windows.first!
+        window.endEditing(true)
+    }
+    
+    // MARK: - action
+    @IBAction func backItemClick(_ sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func userIDTextFieldDidChanged(_ sender: UITextField) {
+        var userId : String = sender.text! as String
+        if userId.count > 20 {
+            let startIndex = userId.index(userId.startIndex, offsetBy: 0)
+            let index = userId.index(userId.startIndex, offsetBy: 20)
+            userId = String(userId[startIndex...index])
+            sender.text = userId;
+        }
+        myUserID = userId;
+    }
+    
+    @IBAction func userNameTextFieldDidChanged(_ sender: UITextField) {
+        var userName = sender.text! as String
+        if userName.count > 32 {
+            let startIndex = userName.index(userName.startIndex, offsetBy: 0)
+            let index = userName.index(userName.startIndex, offsetBy: 32)
+            userName = String(userName[startIndex...index])
+            sender.text = userName
+        }
+        myUserName = userName
+    }
+    
+    @IBAction func loginButtonClick(_ sender: UIButton) {
+        
+        
+        
+    }
+    
+}

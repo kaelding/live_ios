b22f1dfdf9421764116a66984bd097608d8cdab8
Larry Luo <Larryluo@zego.im>
2021-12-30 16:03:35 +0800

    ✨add effects logic

--- ZEGOLiveDemo/ZEGOLiveDemo/AppDelegate.swift
+++ ZEGOLiveDemo/ZEGOLiveDemo/AppDelegate.swift
@@ -13,7 +13,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        RoomManager.shared.initWithAppID(appID: AppCenter.appID(), appSign: AppCenter.appSign()) { result in
+            if result.isFailure {
+                let code = result.failure?.code ?? 1
+                print("init failed: \(String(code))")
+                assert(false, "init app faild")
+            } else {
+                print("[*] Live Room Init Success.")
+            }
+        };
+        
         return true
     }
 

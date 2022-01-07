027dbf6bc1c499033cc0d4bd9fc4942ffe25d776
席航 <careyxi@zego.im>
2022-01-07 19:44:55 +0800

    🚧: change tipview create method -> static.

--- ZEGOLiveDemo/ZEGOLiveDemo/Helper/TipView.swift
+++ ZEGOLiveDemo/ZEGOLiveDemo/Helper/TipView.swift
@@ -18,30 +18,30 @@ class TipView: UIView {
     @IBOutlet weak var messageLabel: UILabel!
     
     var viewType: TipViewType = .warn
+    var autoDismiss: Bool = false
     
-    override class func awakeFromNib() {
+    override func awakeFromNib() {
         super.awakeFromNib()
-        
     }
     
-    func setTipViewType(_ type: TipViewType, message: String) {
-        
-        viewType = type
-        
+    
+    static func showTipView(_ type: TipViewType, message: String, autoDismiss: Bool)  {
+        let tipView: TipView = UINib(nibName: "TipView", bundle: nil).instantiate(withOwner: nil, options: nil).first as! TipView
+        tipView.frame = CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 70)
+        tipView.autoDismiss = autoDismiss
         switch type {
         case .warn:
-            backGroundView.backgroundColor = ZegoColor("BD5454")
+            tipView.backGroundView.backgroundColor = ZegoColor("BD5454")
         case .tip:
-            backGroundView.backgroundColor = ZegoColor("55BC9E")
+            tipView.backGroundView.backgroundColor = ZegoColor("55BC9E")
         }
-        
-        messageLabel.text = message
+        tipView.messageLabel.text = message
+        tipView.show()
     }
     
     func show()  {
-        self.frame = CGRect.init(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 70)
         getKeyWindow().addSubview(self)
-        if viewType == .warn {
+        if autoDismiss {
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
                 self.dismiss()
             }

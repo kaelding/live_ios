e58c2a162d2e78b48acc97f1be22e38fbcbb7333
Kael Ding <415676243@qq.com>
2022-01-20 18:23:32 +0800

    🔧 Config Firebase only under Release.

--- ZEGOLiveDemo/ZEGOLiveDemo/AppDelegate.swift
+++ ZEGOLiveDemo/ZEGOLiveDemo/AppDelegate.swift
@@ -18,7 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        #if canImport(Firebase)
+        #if !DEBUG && canImport(Firebase)
         FirebaseApp.configure()
         #endif
         

9f00dc066315349dbb8fe610691eedb6ca638f61
席航 <careyxi@zego.im>
2022-03-15 16:26:08 +0800

    🚧: init api keep appsign.

--- ZEGOLiveDemo/ZEGOLiveDemo/AppCenter/AppCenter.swift
+++ ZEGOLiveDemo/ZEGOLiveDemo/AppCenter/AppCenter.swift
@@ -19,6 +19,14 @@ struct AppCenter {
         return 0
     }
     
+    static func appSign() -> String {
+        if let dict = getJsonDictionary() {
+            let appSign = dict["appSign"] as! String
+            return appSign
+        }
+        return ""
+    }
+    
     static func serverSecret() -> String {
         if let dict = getJsonDictionary() {
             let appSecret = dict["serverSecret"] as! String

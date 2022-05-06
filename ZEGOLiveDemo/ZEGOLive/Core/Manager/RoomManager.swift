bbb0fe23b16538c4879475916ff583f546b5714f
careyxi <careyxi@zego.im>
2022-05-06 10:47:30 +0800

    ⬆️：update version 1.2.0.

--- ZEGOLiveDemo/ZEGOLive/Core/Manager/RoomManager.swift
+++ ZEGOLiveDemo/ZEGOLive/Core/Manager/RoomManager.swift
@@ -64,7 +64,6 @@ class RoomManager: NSObject {
     /// Call this method at: Before you log in. We recommend you call this method when the application starts.
     ///
     /// @param appID refers to the project ID. To get this, go to ZEGOCLOUD Admin Console: https://console.zegocloud.com/
-    /// @param appSign refers to the secret key for authentication. To get this, please contact us
     func initWithAppID(appID: UInt32, callback: RoomCallback?) {
         ZIMManager.shared.createZIM(appID: appID)
         let profile = ZegoEngineProfile()
